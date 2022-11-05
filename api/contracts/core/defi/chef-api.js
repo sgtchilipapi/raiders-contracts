b036b550efc82dd764a8fd2f1f2cc2b07ec92255
sgtchilipapi <jayemmanuelg@gmail.com>
2022-11-05 13:23:17 +0800

    feat: add api for MiniChefV2

---
+++ api/contracts/core/defi/chef-api.js
@@ -0,0 +1,64 @@
+const networks = require("../../../../app-config/networks")
+const deployments = require("../../../../app-config/deployments")
+const abis = require("../../../../app-config/contract-abis")
+import * as connection from "../../utils/connection"
+import {ethers} from 'ethers'
+
+///contract config
+const network = networks.endpoint.http
+const address = deployments.contracts.defi.minichefv2.address
+const abi = abis.core.chef
+
+///contract connections
+async function getContract(){
+    const contract = await connection.getContractInstance(network, address, abi)
+    return contract
+}
+
+async function getSignedContract(){
+    const contract = await connection.getSignedContractInstance(network, address, abi)
+    return contract
+}
+
+///view functions
+export async function poolInfo(pool_id){
+    const contract = await getContract()
+    const pool = await contract.poolInfo(pool_id)
+    return pool
+}
+
+export async function userInfo(pool_id, address){
+    const contract = await getContract()
+    const user = await contract.userInfo(pool_id, address)
+    return user
+}
+
+///transaction functions
+export async function deposit(pool_id, amount, addressTo){
+    const contract = await getSignedContract()
+    const depositTx = await contract.deposit(pool_id, amount, addressTo)
+    const receipt = depositTx.wait()
+    return receipt
+}
+
+export async function withdraw(pool_id, amount, addressTo){
+    const contract = await getSignedContract()
+    const withdrawTx = await contract.withdraw(pool_id, amount, addressTo)
+    const receipt = withdrawTx.wait()
+    return receipt
+}
+
+export async function harvest(pool_id, addressTo){
+    const contract = await getSignedContract()
+    const harvestTx = await contract.harvest(pool_id, addressTo)
+    const receipt = harvestTx.wait()
+    return receipt
+}
+
+export async function withdrawAndHarvest(pool_id, amount, addressTo){
+    const contract = await getSignedContract()
+    const withdrawTx = await contract.withdrawAndHarvest(pool_id, amount, addressTo)
+    const receipt = withdrawTx.wait()
+    return receipt
+}
+
